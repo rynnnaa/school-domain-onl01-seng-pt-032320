e4d726c1fc321988d777006019dddbb6be102813
Ryna Truong <rynatruong@outlook.com>
2020-05-01 21:33:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    roster
+  end
+end
